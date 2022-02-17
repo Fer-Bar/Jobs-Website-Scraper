6f9301b5fdc0c0778827f97cd61cc7276ff9e8d2
Fer-Bar <linofernando2703@gmail.com>
2022-02-17 20:25:23 -0300

    Added the requirements.txt

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-# Le preguntamos al usuario las tecnologías con las que no este 
-# familiarizado para filtrar las busquedas
-unfamiliar_skills = input('Put some skills that you are not familiar with: ').split(' ')
+# Le preguntamos al usuario las tecnologías con las que no este familiarizado
+# para filtrar ofertas de empleo adecuadas a las habilidades del usuario
+unfamiliar_skills = input('Put some skills that you are not familiar with: ').split(' ') # Use spaces between every unfamiliar skill that you have
 print(f'Filrering out {unfamiliar_skills}...')
 
 def find_jobs():
@@ -19,8 +19,9 @@ def find_jobs():
             company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
             skills = job.find('span', class_='srp-skills').text.replace(' ', '')
             more_info = job.header.h2.a['href']
-
-            skills_list = skills.strip().split(',')
+            # Ponemos todas las habilidades requeridas en una lista
+            skills_list = skills.strip().split(',') 
+            # Si no esta familiarizado con x tecnología requirida en la oferta de trabajo se descarta 
             if (all(unf_skill not in skills_list for unf_skill in unfamiliar_skills)):
                 with open(f'posts/{index}.txt', 'w') as f:
                     f.write(f'Company Name: {company_name.strip()} \n')
@@ -31,6 +32,6 @@ def find_jobs():
 if __name__ == '__main__':
     while True:
         find_jobs()
-        time_wait = 60
+        time_wait = 60 # El formato es en minutos.
         print(f'Waiting {time_wait} minutes for the next scrap...')
         time.sleep(time_wait * 60)
