dcbc2457fbfd77fface2b22449d6fe162438b035
Fer-Bar <linofernando2703@gmail.com>
2022-02-17 18:50:01 -0300

    Added a function who accept many unfamiliar skills

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Jobs-Website-Scraper
+
+## The functionalities we will add:
+ - Show a nicer output of the results
+ - Option to filter out jobs that do not meet some skills that are required.
+ - Scraping the website every n(10 by default) minutes
+ - Writing the results to separated text files.
---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+# Le preguntamos al usuario las tecnologías con las que no este 
+# familiarizado para filtrar las busquedas
+unfamiliar_skills = input('Put some skills that you are not familiar with: ').split(' ')
+print(f'Filrering out {unfamiliar_skills}...')
+
+def find_jobs():
+    url = ('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=')
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'lxml')
+
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    for index, job in enumerate(jobs):
+        published_date = job.find('span', class_='sim-posted').span.text
+        if 'few' in published_date:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+            more_info = job.header.h2.a['href']
+
+            skills_list = skills.strip().split(',')
+            if (all(unf_skill not in skills_list for unf_skill in unfamiliar_skills)):
+                with open(f'posts/{index}.txt', 'w') as f:
+                    f.write(f'Company Name: {company_name.strip()} \n')
+                    f.write(f'Skills required: {skills.strip()} \n')
+                    f.write(f'More Info: {more_info} ')
+                print(f'File Saved: {index} ')
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes for the next scrap...')
+        time.sleep(time_wait * 60)
